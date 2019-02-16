588eb57e737b288a831190d9f38ddd82aeaf5258
zfz <zhoufangzhang@gmail.com>
2019-02-16 18:36:21 +0000

    Add code template

---
+++ main.py
@@ -0,0 +1,56 @@
+import io
+import os
+import sys
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+from google.gax.errors import RetryError
+# import google.auth
+from google.oauth2 import service_account
+
+from flask import Flask, render_template, request, json
+
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+
+socketio = SocketIO(app)
+
+# credentials, project = google.auth.default()
+credentials = service_account.Credentials.from_service_account_file(
+    'Shama-1a035578a669.json')
+credentials = credentials.with_scopes(
+    ['https://www.googleapis.com/auth/cloud-platform'])
+
+client = speech.SpeechClient(credentials=credentials)
+
+
+def processData(data):
+    content = data
+    audio = types.RecognitionAudio(content=content)
+
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=48000,
+        language_code='en-US')
+
+    try:
+        response = client.recognize(config=config, audio=audio)
+        for result in response.results:
+            emit('transcript', result.alternatives[0].transcript)
+    except RetryError as e:
+        print("Error: {0}".format(e))
+    except:
+        print("Error:", sys.exc_info()[0])
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@socketio.on('stream')
+def handle_stream(blob):
+    processData(blob)
+
+if __name__ == "__main__":
+    socketio.run(app, '0.0.0.0')
---
+++ static/js/shama.js
@@ -0,0 +1,81 @@
+$( document ).ready( function () {
+  var shama = {};
+
+  shama.init = function () {
+    shama.createWebSocket();
+    shama.initRecordButton();
+  };
+
+  shama.getRecorder = function ( s ) {
+    context = new AudioContext();
+    source = context.createMediaStreamSource( s );
+    recorder = new Recorder( source, {
+      numChannels: 1,
+    } );
+    recorder.record();
+    shama.setInterval();
+  };
+
+  shama.createWebSocket = function () {
+    socket = io.connect( '//' + document.domain + ':' + location.port );
+    socket.on( 'connect', function () {
+      console.log( 'Connected!' );
+    } );
+
+    socket.on( 'transcript', function ( data ) {
+      $( '.recognized-text' ).html( data );
+    } );
+  };
+
+  shama.setInterval = function () {
+    if ( 'undefined' !== typeof recorder ) {
+      exportInterval = setInterval( function () {
+        recorder.exportWAV( function ( blob ) {
+          recorder.clear();
+          if ( 'undefined' !== typeof socket ) {
+            socket.emit( 'stream', blob );
+          }
+        } );
+      }, 5000 );
+    }
+  };
+
+  shama.clearInterval = function () {
+    clearInterval( exportInterval );
+  };
+
+  shama.initRecorder = function () {
+    if ( 'undefined' === typeof recorder ) {
+      navigator.mediaDevices.getUserMedia( {
+        audio: true,
+        video: false
+      } ).then( shama.getRecorder );
+    }
+  };
+
+  shama.initRecordButton = function () {
+    $( '.recorder' ).click( function ( e ) {
+      e.preventDefault();
+
+      if ( $( this ).hasClass( 'is-recording' ) ) { // Stop Recording
+        $( this ).removeClass( 'active is-recording btn-danger' ).text( 'Start Recording' );
+        if ( 'undefined' !== typeof recorder ) {
+          recorder.stop();
+        }
+        shama.clearInterval();
+      }
+      else { // Start Recording
+        $( this ).addClass( 'active is-recording btn-danger' ).text( 'Stop Recording' );
+        if ( 'undefined' !== typeof recorder ) {
+          recorder.record();
+          shama.setInterval();
+        }
+        else {
+          shama.initRecorder();
+        }
+      }
+    } );
+  };
+
+  $( shama.init() );
+} );
